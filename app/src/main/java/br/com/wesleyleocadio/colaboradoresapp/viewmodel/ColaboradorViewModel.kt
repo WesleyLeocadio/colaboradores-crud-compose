3738b4454096b4468d4256e0e9acd6f0ff22f414
Wesley Leocadio <silvawesley374@gmail.com>
2026-06-25 19:12:27 -0300

    feat: implementar ViewModel com operações de CRUD de colaboradores

--- app/src/main/java/br/com/wesleyleocadio/colaboradoresapp/viewmodel/ColaboradorViewModel.kt
+++ app/src/main/java/br/com/wesleyleocadio/colaboradoresapp/viewmodel/ColaboradorViewModel.kt
@@ -1,4 +1,26 @@
 package br.com.wesleyleocadio.colaboradoresapp.viewmodel
 
-class ColaboradorViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import br.com.wesleyleocadio.colaboradoresapp.model.Colaborador
+
+
+class ColaboradorViewModel : ViewModel() {
+
+    val colaboradores = mutableStateListOf<Colaborador>()
+
+    fun adicionar(colaborador: Colaborador) {
+        colaboradores.add(colaborador)
+    }
+
+    fun remover(colaborador: Colaborador) {
+        colaboradores.remove(colaborador)
+    }
+
+    fun atualizar(colaboradorAtualizado: Colaborador) {
+        val index = colaboradores.indexOfFirst { it.id == colaboradorAtualizado.id }
+        if (index != -1) {
+            colaboradores[index] = colaboradorAtualizado
+        }
+    }
 }
